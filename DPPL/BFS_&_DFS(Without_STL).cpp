c2bfdc034c16c9cc5c0c5e01ab4acfa6e778b774
Anikesh Pal <31801904+anix00011@users.noreply.github.com>
2019-03-09 01:39:33 +0530

    Update BFS_&_DFS(Without_STL).cpp

--- DPPL/BFS_&_DFS(Without_STL).cpp
+++ DPPL/BFS_&_DFS(Without_STL).cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#define vert1 1000
+#define vert2 100
 using namespace std;
 bool chk=false;
-bool *visited=new bool[1000];
+bool *visited=new bool[vert1];
 class Node {
     public:
     int data;
@@ -10,7 +12,7 @@ class Node {
 class Graph{
     public:
     int Vertex,Edge;
-    bool Adj[100][100];
+    bool Adj[vert2][vert2];
 };
 Node* newNode(int data){
     Node *tmp=new Node;
@@ -32,6 +34,28 @@ Node* pop_queue(Node* root){
     return root;
 }
 int peek_queue(Node* root){
+    return root->data;
+}
+Node* pop_stack(Node* root){
+    int i=0;
+    if(root==NULL) return NULL;
+    Node* temp=NULL;
+    Node* temp2=NULL;
+    temp = root;
+    if(temp->next==NULL){
+        root=NULL;
+        return root;
+    }
+    while(temp->next != NULL){
+        temp2=temp;
+        temp = temp->next;
+        i++;
+    }
+    temp2->next=NULL;
+    return root;
+}
+int peek_stack(Node* root){
+    while(root->next != NULL) root=root->next;
     return root->data;
 }
 int MV(Graph *Grph,int St_Root){
@@ -92,6 +116,80 @@ void Left_Node(bool *visited,Graph *Grph){
             visited[i]=true;
         }
 }
+Node* topo_Sort(int St_Root,Node* Stack,Graph *Grph){
+    visited[St_Root]=true;
+    for(int i=0;i<Grph->Vertex;i++) if(!visited[i]&&Grph->Adj[St_Root][i]) Stack=topo_Sort(i,Stack,Grph);
+    Stack=push(Stack,St_Root);
+    return Stack;
+}
+
+void topo_SortUtil(Graph *Grph){
+    Node* Stack=NULL;
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    for(int i=0;i<Grph->Vertex;i++) if(!visited[i]) Stack=topo_Sort(i,Stack,Grph);
+    while(!isempty(Stack)){
+        cout<<peek_stack(Stack)<<" ";
+        Stack=pop_stack(Stack);
+    }
+}
+void maxEdge(Graph *Grph){
+    Node* Stack=NULL;
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    for(int i=0;i<Grph->Vertex;i++) if(!visited[i]) Stack=topo_Sort(i,Stack,Grph);
+    int tsort[Grph->Vertex];
+    for(int i=0;i<Grph->Vertex;i++){
+        tsort[i]=peek_stack(Stack);
+        Stack=pop_stack(Stack);
+    }
+    int c=0;
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    for(int i=0;i<Grph->Vertex;i++){
+        int t=tsort[i];
+        for(int j=0;j<Grph->Vertex;j++) if(Grph->Adj[t][j]) visited[j]=true;
+        for(int j=i+1;j<Grph->Vertex;j++){
+            if(!visited[tsort[j]]) c++;
+            visited[tsort[j]]=false;
+        }
+    }
+    cout<<c;
+}
+bool isCyclicUtilD(int x,bool *r,Graph *Grph){
+    if(!visited[x]){
+        visited[x]=true;
+        r[x]=true;
+        for(int i=0;i<Grph->Vertex;i++){
+            if(!visited[i]&&Grph->Adj[x][i]&&isCyclicUtilD(i,r,Grph)) return true;
+            else if(r[i]&&Grph->Adj[x][i]) return true;
+        }
+    }
+    r[x]=false;
+    return false;
+}
+bool isCyclicD(Graph *Grph){
+    bool *r=new bool[Grph->Vertex];
+    for(int i=0;i<Grph->Vertex;i++){
+        visited[i]=false;
+        r[i]=false;
+    }
+    for(int i=0;i<Grph->Vertex;i++) if(isCyclicUtilD(i,r,Grph)) return true;
+    return false;
+}
+bool bipartiteUtil(int St_Root,int *color,Graph *Grph,int c=0){
+    color[St_Root]=c%2;
+    for (int i=0;i<Grph->Vertex;i++){
+        if (Grph->Adj[St_Root][i]||Grph->Adj[i][St_Root]&&i!=St_Root){
+            if(color[i]==color[St_Root]) return false;
+            if(color[i]==-1) if (!bipartiteUtil(i,color,Grph,c+1)) return false;
+        }
+    }
+    return true;
+}
+bool bipartite(Graph *Grph){
+    int color[Grph->Vertex];
+    for(int i=0;i<Grph->Vertex;i++) color[i]=-1;
+    for(int i=0;i<Grph->Vertex;i++) if (color[i]==-1) if (!bipartiteUtil(i, color,Grph)) return false;
+    return true;
+}
 int main(){
     Graph *Grph=new Graph;
     cout<<"Enter the number of Nodes and Edges:\n";
@@ -102,6 +200,7 @@ int main(){
     for(int i=0;i<Grph->Edge;i++){
         cin>>x>>y;
         Grph->Adj[x][y]=true;
+        //Grph->Adj[y][x]=true;
     }
     for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
     int val=-1;
@@ -116,4 +215,22 @@ int main(){
     cout<<"\nDFS from vertex "<<val<<" is: ";
     DFS(Grph,2);
     Left_Node(visited,Grph);
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    cout<<"\nTopologically Sorted Graph: ";
+    topo_SortUtil(Grph);
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    cout<<"\nMax Edges: ";
+    maxEdge(Grph);
+    if(isCyclicD(Grph)) cout<<"\nGraph is Cyclic";//For Directed
+    else cout<<"\nGraph is not Cyclic";
+    //if(bipartite(Grph)) cout<<"\nGraph is bipartite";
+    //else cout<<"\nGraph is not bipartite";//Use only for Undirected Graph
+    int cx=0;
+    for(int i=0;i<Grph->Vertex;i++) visited[i]=false;
+    Node* xx=path(6,1,Grph);
+    cout<<"\nPath: \n";
+    while(!isempty(xx)){
+        cout<<peek_stack(xx)<<" ";
+        xx=pop_stack(xx);
+    }
 }
